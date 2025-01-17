ead5bdba5abc62849eae269c91839e5ad5b44fda
Lussskki <lussskki@gmail.com>
2025-01-17 16:17:36 +0400

    add fps

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from rectangle import Rectangle
 
 pygame.init()
 
-color_white = (255, 255, 255)
+color_white_sky = (0, 0, 255)
 rect_color_red = (255, 0, 0)
 
 game_surface = pygame.display.set_mode((700, 500))
 pygame.display.set_caption('Game')
 
-rect = Rectangle(500, 400, 30, 30, rect_color_red, 2, 700, 500)
+rect = Rectangle(500, 400, 50, 50, rect_color_red, 2, 700, 500)
+way = Rectangle(50, 250, 300, 30, 'green',2, 700,500 )
+
+# FPS
+fps=pygame.time.Clock()
 
 gamepad = Gamepad(dead_zone=0.1)
 keyboard = Keyboard()
@@ -19,13 +23,18 @@ keyboard = Keyboard()
 exit = False
 
 while not exit:
-    game_surface.fill(color_white)
+    fps.tick(60)
+    # Game surface, keyboard and gamepad events
+    game_surface.fill(color_white_sky)
     exit = gamepad.process_events() or keyboard.process_events()
     
+    # Movement of rect 
     left_stick_x, left_stick_y = gamepad.get_movement()
     key_x, key_y = keyboard.get_movement()
     
+    # Draw on surface
     rect.update_position(left_stick_x + key_x, left_stick_y + key_y)
     rect.draw(game_surface)
+    way.draw(game_surface)
 
     pygame.display.update()
