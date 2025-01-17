626690ec1498fe288df4aba8f340ad90df85b561
Lussskki <lussskki@gmail.com>
2025-01-17 11:35:11 +0400

    feat: add keyboard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from gamepad import Gamepad
+from keyboard import Keyboard
 from rectangle import Rectangle
 
 pygame.init()
@@ -8,27 +9,23 @@ color_white = (255, 255, 255)
 rect_color_red = (255, 0, 0)
 
 game_surface = pygame.display.set_mode((700, 500))
-pygame.display.set_caption('Surface')
+pygame.display.set_caption('Game')
 
 rect = Rectangle(500, 400, 30, 30, rect_color_red, 2, 700, 500)
 
 gamepad = Gamepad(dead_zone=0.1)
+keyboard = Keyboard()
 
 exit = False
 
 while not exit:
     game_surface.fill(color_white)
-
-    # Process gamepad events
-    exit = gamepad.process_events()
-
-    # Get joystick movement
+    exit = gamepad.process_events() or keyboard.process_events()
+    
     left_stick_x, left_stick_y = gamepad.get_movement()
-
-    # Update rectangle position
-    rect.update_position(left_stick_x, left_stick_y)
-
-    # Draw rectangle
+    key_x, key_y = keyboard.get_movement()
+    
+    rect.update_position(left_stick_x + key_x, left_stick_y + key_y)
     rect.draw(game_surface)
 
     pygame.display.update()
